55fef74a134681f2ebfcec9875eb4cbde7896373
Dylan Jeffers <sapientech@openmailbox.org>
2016-10-25 18:24:10 -0700

    arguile: Add fn

--- arguile.scm
+++ arguile.scm
@@ -1,5 +1,5 @@
 (define-module (arguile))
-(export mac def module use)
+(export mac def module use fn)
 
 ;;; TODO allow mac to not require double parens
 (define-syntax mac
@@ -14,10 +14,15 @@
 (mac def
   ((def name (arg ...) exp rest ...)
    (define (name arg ...) exp rest ...))
-  ((def name val) (define name val)))
+  ((def name val)
+   (define name val)))
 
 (mac module
   ((module (base ext ...)) (define-module (base ext ...)))
   ((module base ext ...) (module (base ext ...))))
 
 (mac use ((use modules) (use-modules modules)))
+
+(mac fn
+  ((fn (args ...) body body* ...)
+   (lambda (args ...) body body* ...)))
