2d0371a2a977fb1b4c1a221295440e1db1400834
Dylan Jeffers <sapientech@openmailbox.org>
2016-11-04 19:01:33 -0700

    Update mac, remove syn
    Combined mac and syn into an all powerful macro :)

--- arguile/ssyntax.scm
+++ arguile/ssyntax.scm
@@ -2,19 +2,20 @@
   #:export (mac syn syn-case w/syn))
 
 (define-syntax mac
-  (syntax-rules ()
-    ((mac kword ((_ . patt) templ) ...)
-     (mac kword () ((_ . patt) templ) ...))
-    ((mac kword (aux ...) ((_ . patt) templ) ...)
-     (define-syntax kword
-         (lambda (ctx)
-           (syntax-case ctx (aux ...)
-             ((_ . patt) templ) ...))))))
-(mac syn
-     ;; Maybe change name to syntax
-  ((_ name (ctx) body)
-   #'(define-syntax name
-       (lambda (ctx) body))))
+  (lambda (ctx)
+    (syntax-case ctx ()
+      ((mac name ((_ . patt) templ) ...)
+       #'(mac name x () ((_ . patt) templ) ...))
+      ((mac name x ((_ . patt) templ) ...)
+       (identifier? #'x)
+       #'(mac name x () ((_ . patt) templ) ...))
+      ((mac name (aux ...) ((_ . patt) templ) ...)
+       #'(mac name x (aux ...) ((_ . patt) templ) ...))
+      ((mac name x (aux ...) ((_ . patt) templ) ...)
+       #'(define-syntax name
+           (lambda (x)
+             (syntax-case x (aux ...)
+               ((_ . patt) templ) ...)))))))
 
 (mac syn-case
      ;; May not handle all cases
@@ -28,4 +29,4 @@
   ((_ (syn val) e1 e2 ...)
    #'(with-syntax ((syn val)) e1 e2 ...))
   ((_ (syn val rest ...) e1 e2 ...)
-   #'(with-syntax ((syn val)))))
+   #'(with-syntax ((syn val)) (w/syn (rest ...) e1 e2 ...))))
