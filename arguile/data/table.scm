bbe2101641a51bee70f6c279591dcaa9c9b82cad
Dylan Jeffers <sapientech@openmailbox.org>
2016-11-09 23:04:32 -0800

    Update table, now much cleaner

--- arguile/data/table.scm
+++ arguile/data/table.scm
@@ -1,23 +1,10 @@
 (define-module (arguile data table)
-  #:use-module (arguile)
-  #:use-module (oop goops)
   #:export (make-table))
-                                        ;TODO: put these in class module
-(mac class
-  ((class name supers slot ...) (define-class name supers slot ...)))
+(use-modules (arguile ssyntax)
+             (arguile data))
 
-(mac inst
-  ((inst name class initargs ...) (def name (make class initargs ...))))
-
-(class table (<applicable-struct>)
-  (procedure #:init-value (fn (o) (display o)))
-  (%table #:init-value (make-hash-table)))
-                                        ;TODO: Add optional size arg
-(def make-table ()
- (inst tble table)
- (slot-set! tble 'procedure
-            (let t (slot-ref tble '%table)
-              (case-lambda
-                ((k) (hash-ref t k))
-                ((k v) (hash-set! t k v)))))
- tble)
+(data table () ()
+      (let t (make-hash-table)
+        (case-lambda
+          ((k) (hash-ref t k))
+          ((k v) (hash-set! t k v)))))
