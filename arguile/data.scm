cc442b16694f63aa133fb65a0a8777552f6ad79c
Dylan Jeffers <sapientech@openmailbox.org>
2016-11-09 22:58:43 -0800

    Update data, move case-lambda into data macro
    Still need anaphoric self to make case-lambda and constructor more
    useful.

--- arguile/data.scm
+++ arguile/data.scm
@@ -16,16 +16,18 @@
   (or (record? obj)
       (eq? (struct-vtable obj) <data>)))
 
+;;; Need anaphoric self for applicable lambda 
+;;; Lets hold off from all the features until we implement def*
 (mac data x
-  ((_ name (fields ...) (methods ...) proc? ...)
-   (with (proc? (not (null? (syn->dat #'(proc? ...))))
-          name' (syn->dat #'name))
-     (w/syn (pred (dat->syn x (+ name' '?))
-                  const (dat->syn x (+ 'make- (syn->dat #'name)))
-                  %const (dat->syn x (+ '%make- name')))
-       #`(do (define-record-type name
-               (#,(if proc? #'%const #'const) fields ...)
-               pred methods ...)
-           #,(when proc?
-               #'(def const (proc)
-                   (make-struct <data> 0 proc))))))))
+     ((_ name (fields ...) (methods ...) proc ...)
+      (with (proc? (not (null? (syn->dat #'(proc ...))))
+                   name' (syn->dat #'name))
+        (w/syn (pred (dat->syn x (+ name' '?))
+                     const (dat->syn x (+ 'make- name'))
+                     %const (dat->syn x (+ '%make- name')))
+          #`(do (define-record-type name
+                  (#,(if proc? #'%const #'const) fields ...)
+                  pred methods ...)
+              #,(when proc?
+                  #'(def const ()
+                      (make-struct <data> 0 proc ...))))))))
