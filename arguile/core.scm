069c3a54433672b1d483ee0f4bb83087bb9951b0
Dylan Jeffers <sapientech@openmailbox.org>
2016-11-14 11:10:33 -0800

    Add &, fn-case

--- arguile/core.scm
+++ arguile/core.scm
@@ -1,11 +1,12 @@
 (module (arguile core)
-  #:export (fn def let with do = =? 0? 1?))
+  #:export (fn def let with do fn-case & = =? 0? 1?))
 (use (arguile guile)
      (arguile ssyntax))
 (mac fn
   ((_ args e1 e2 ...)
    #'(lambda args e1 e2 ...)))
 
+;;; Add/integrate def*
 (mac def
   ((_ name args e1 e2 ...)
    #'(define name (fn args e1 e2 ...)))
@@ -25,13 +26,18 @@
    #'(_let ((var val)) (with (rest ...) e1 e2 ...))))
 
 (mac do ((_ e1 ...) #'(begin e1 ...)))
-
 ;;; TODO: check if var is a free variable, and if so, define it
 (mac =
   ((_ var val) #'(set! var val))
   ((_ var val rest ...)
    #'(do (set! var val)
          (= rest ...))))
+
+(mac fn-case
+  ((_ fn1 fn2 ...) #'(case-lambda fn1 fn2 ...)))
+
+(mac &
+  ((_ e1 ...) #'(and e1 ...)))
 
 (def =? _=)
 (def 0? zero?)
