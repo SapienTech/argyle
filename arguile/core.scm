b9056b0e458fbba0724c6391f19e7f5013e42259
Dylan Jeffers <sapientech@openmailbox.org>
2016-11-14 23:28:02 -0800

    Fix indentation

--- arguile/core.scm
+++ arguile/core.scm
@@ -20,36 +20,36 @@
   ((_ name val)
    #'(define name val)))
 
-  (mac let
-    ((_ var val e1 e2 ...)
-     #'(with (var val) e1 e2 ...)))
+(mac let
+  ((_ var val e1 e2 ...)
+   #'(with (var val) e1 e2 ...)))
 
-  (mac with
-    ((_ () e1 e2 ...)
-     #'(_let () e1 e2 ...))
-    ((_ (var val) e1 e2 ...)
-     #'(_let ((var val)) e1 e2 ...))
-    ((_ (var val rest ...) e1 e2 ...)
-     #'(_let ((var val)) (with (rest ...) e1 e2 ...))))
+(mac with
+  ((_ () e1 e2 ...)
+   #'(_let () e1 e2 ...))
+  ((_ (var val) e1 e2 ...)
+   #'(_let ((var val)) e1 e2 ...))
+  ((_ (var val rest ...) e1 e2 ...)
+   #'(_let ((var val)) (with (rest ...) e1 e2 ...))))
 
-  (mac do ((_ e1 ...) #'(begin e1 ...)))
+(mac do ((_ e1 ...) #'(begin e1 ...)))
 ;;; TODO: check if var is a free variable, and if so, define it
-  (mac =
-    ((_ var val) #'(set! var val))
-    ((_ var val rest ...)
-     #'(do (set! var val)
-         (= rest ...))))
+(mac =
+  ((_ var val) #'(set! var val))
+  ((_ var val rest ...)
+   #'(do (set! var val)
+       (= rest ...))))
 
-  (mac fn-case
-    ((_ fn1 fn2 ...) #'(case-lambda fn1 fn2 ...)))
+(mac fn-case
+  ((_ fn1 fn2 ...) #'(case-lambda fn1 fn2 ...)))
 
-  (mac &
-    ((_ e1 ...) #'(and e1 ...)))
+(mac &
+  ((_ e1 ...) #'(and e1 ...)))
 
 ;;; Make generic
-  (def =? _=)
-  (def 0? zero?)
-  (def 1? (n) (=? 1 n))
+(def =? _=)
+(def 0? zero?)
+(def 1? (n) (=? 1 n))
 
 (def expand-kwargs (args ctx)
   (loop ((for arg (in-list args))
