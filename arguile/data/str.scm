0a444cf0d3586b04e0b6c64b0a01b66c29948e4b
Dylan Jeffers <sapientech@openmailbox.org>
2016-11-15 00:05:54 -0800

    Add popular str fns

---
+++ arguile/data/str.scm
@@ -0,0 +1,20 @@
+(module (arguile data str))
+(use (arguile core))
+
+;;; Standardize a macro for ref, set, copy, etc
+(defp str string)
+(defp str? string?)
+(defp str: string-ref)
+(defp str! string-set!)
+(defp str-join string-append)
+(defp str->lst string->list)
+(defp str->num string->number)
+(defp str->sym string->symbol)
+(defp str->chr-set string->char-set)
+(defp str-cpy string-copy)
+(defp str-cpy! string-copy!)
+(defp str-map string-map)
+(defp str-fold string-fold)
+(defp str-take string-take)
+(defp str-drop string-drop)
+
