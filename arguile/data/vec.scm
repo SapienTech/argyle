a9f1f59fe6bf2ab3d143bf099d5cf4135a7ec110
Dylan Jeffers <sapientech@openmailbox.org>
2016-11-16 15:31:15 -0800

    Fix defs to work with vecs, not vectors

--- arguile/data/vec.scm
+++ arguile/data/vec.scm
@@ -2,7 +2,8 @@
   #:export (make-vec vec-fn vec-fn!
             vec? vec-len))
 (use (arguile base)
-     (arguile data))
+     (arguile data)
+     (arguile generic))
 
 ;;; TODO: add optional fill
 (data vec (len) ((len vec-len))
@@ -12,11 +13,24 @@
      ((k) (vector-ref v k))
      ((k obj) (vector-set! v k obj)))))
 
-(defp vec make-vec)
 (defp vec: (v k) (v k))
 (defp vec! (v k obj) (v k obj))
 (defp vec->lst (v) (vector->list (v)))
-(defp vec-cpy (v) (vector-copy (v)))
+;;; TODO: this is a bit verbose, but will change when
+;;; we can have op args
+(defp lst->vec (lst) (ret v (make-vec (len lst))
+                          (vec-obj! v (list->vector lst))))
+(defp vec-cpy (v) (ret v* (make-vec (vec-len v))
+                       (vec-obj! v* (vector-copy (v)))))
 (defp vec-fill! (v fill) (vector-fill! (v) fill))
 (defp vec<-! (v1 s1 e1 v2 s2) (vector-move-left! (v1) s1 e1 (v2) s2))
 (defp vec->! (v1 s1 e1 v2 s2) (vector-move-right! (v1) s1 e1 (v2) s2))
+
+;;; Temporary helper
+(def vec-obj! (v obj)
+  (vec-fn! v
+    (let v* obj
+      (fn-case
+       (() v*)
+       ((k) (vector-ref v* k))
+       ((k obj) (vector-set! v* k obj))))))
