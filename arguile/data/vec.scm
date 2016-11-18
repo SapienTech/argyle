fba7712f2a0446540ae18c655a4c4fd20b402096
Dylan Jeffers <sapientech@openmailbox.org>
2016-11-18 14:02:20 -0800

    Update vec w/ new data scheme

--- arguile/data/vec.scm
+++ arguile/data/vec.scm
@@ -1,36 +1,25 @@
 (module (arguile data vec)
-  #:export (vec make-vec vec-fn vec-fn!
-            vec? vec-len))
+  #:export (vec vec? vec-fn vec-fn!))
 (use (arguile base)
-     (arguile data)
-     (arguile generic))
+     (arguile data))
 
 ;;; TODO: add optional fill
-(data vec (len) ((len vec-len))
-  (let v (make-vector (vec-len self))
-    (fn-case
-     (() v)
-     ((k) (vector-ref v k))
-     ((k obj) (vector-set! v k obj)))))
+(data vec (v)
+      #:init (%make-vec v)
+      #:app (let v (vec-v self)
+              (fn-case
+               (() v)
+               ((k) (vector-ref v k))
+               ((k obj) (vector-set! v k obj)))))
 
+(defp make-vec (len . fill)
+  (%make-vec (if (nil? fill) (make-vector len)
+                 (make-vector len (car fill)))))
 (defp vec: (v k) (v k))
 (defp vec! (v k obj) (v k obj))
 (defp vec->lst (v) (vector->list (v)))
-;;; TODO: this is a bit verbose, but will change when
-;;; we can have op args
-(defp lst->vec (lst) (ret v (make-vec (len lst))
-                          (vec-obj! v (list->vector lst))))
-(defp vec-cpy (v) (ret v* (make-vec (vec-len v))
-                       (vec-obj! v* (vector-copy (v)))))
+(defp lst->vec (lst) (%make-vec (list->vector lst)))
+(defp vec-cpy (v) (%make-vec (vector-copy (v))))
 (defp vec-fill! (v fill) (vector-fill! (v) fill))
 (defp vec<-! (v1 s1 e1 v2 s2) (vector-move-left! (v1) s1 e1 (v2) s2))
 (defp vec->! (v1 s1 e1 v2 s2) (vector-move-right! (v1) s1 e1 (v2) s2))
-
-;;; Temporary helper
-(def vec-obj! (v obj)
-  (vec-fn! v
-    (let v* obj
-      (fn-case
-       (() v*)
-       ((k) (vector-ref v* k))
-       ((k obj) (vector-set! v* k obj))))))
