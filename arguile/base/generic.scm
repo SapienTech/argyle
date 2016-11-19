9a0d3fe196b1c34f5756142f4bf3b77f7d6b189e
Dylan Jeffers <sapientech@openmailbox.org>
2016-11-18 22:15:01 -0800

    Move (arguile generic) -> (arguile base generic)
    Remove generic, extend (making way for new (arguile generic)

---
+++ arguile/base/generic.scm
@@ -0,0 +1,37 @@
+(module (arguile base generic)
+  #:export (+ * len))
+(use (arguile base)
+     (arguile guile)
+     (arguile type)) 
+
+(def + args
+  (cond ((null? args) 0)
+        ((one-of `(,str? ,chr?) (car args))
+         (apply str-join (map (\\ -> str _) args)))
+        ((sym? (car args))
+         (apply sym-join (map (\\ -> sym _) args)))
+        (else (apply _+ args))))
+
+;;; TODO: Add cartesian product for data
+(def * args
+  (cond ((null? args) 0)
+        ((one-of `(,str? ,chr?) (car args))
+         (apply str-join
+                (map (fn (val) (-> str (car args)))
+                     (iota (apply _* (cdr args))))))
+        ((sym? (car args))
+         (apply sym-join
+                (map (fn (val) (-> sym (car args)))
+                     (iota (apply _* (cdr args))))))
+        (else (apply _* args))))
+
+(def len (x)
+  (cond ((str? x) (str-len x))
+        ((hash-table? x) (hash-count (const #t) x))
+        ((vector? x) (vector-length x))
+        (else (length x))))
+
+(def one-of (tests val)
+  (if (null? tests) #f
+      (or ((car tests) val)
+          (one-of (cdr tests) val))))
