e6a736837d44fea67c0b1bc67782628cf80cbf66
Dylan Jeffers <sapientech@openmailbox.org>
2016-11-19 21:20:31 -0800

    Update extend, now handles (a str b num) param lst

--- arguile/generic.scm
+++ arguile/generic.scm
@@ -1,12 +1,10 @@
 (module (arguile generic)
     #:export (generic extend))
 (use (arguile base)
-     (arguile loop)
-     (arguile type)
      (arguile data)
-     (arguile error)
-     (arguile io)
-     (arguile data tbl))
+     (arguile guile)
+     (arguile loop)
+     (srfi srfi-1))
 
 (data gen-fn (name tbl)
       #:init (%gen-fn name tbl)
@@ -18,20 +16,24 @@
   ((_ name) (id? #'name)
    #'(def name (%gen-fn 'name (make-tbl)))))
 
-;;; Will overwrite defs w/ same args
-;;; args must be values, not variables
-;;; fn must be a full proc
 (mac extend
-  ((_ name (arg1 ...) fn)
+  ((_ name (arg1 ...) body ...)
    (defd? (-> dat #'name))
-   #'(loop ((for arg (in-list '(arg1 ...)))
-            (where tbl (gen-fn-tbl name)
-              (with (arg-t (type arg) t (tbl arg-t))
-                (if t t (do (tbl arg-t (make-tbl))
-                          (tbl arg-t))))))
-        => (tbl 'fun fn))))
+   (w/syn ((args types) (split #'(arg1 ...)))
+     #'(loop ((for type (in-list 'types))
+              (where tbl (gen-fn-tbl name)
+                (let tbl* (tbl type)
+                  (if tbl* tbl*
+                      (do (tbl type (make-tbl))
+                          (tbl type))))))
+         => (tbl 'fn (fn args body ...))))))
 
 (def resolve-fn (tbl args)
   (loop ((for arg (in-list args))
          (where t tbl (and=> t (\\ _ (type arg)))))
-        => (if t (t 'fun) (err "No generic function for args:" args))))
+    => (if t (t 'fn) (err "No generic function for args:" args))))
+
+(eval-when (expand load eval)
+  (def split (lst)
+    (call-with-values (fn () (unzip2 (grp lst 2)))
+      list)))
