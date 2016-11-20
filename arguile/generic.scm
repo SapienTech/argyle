54060d9842c167b113d64d4b3dd5ad3732ce3715
Dylan Jeffers <sapientech@openmailbox.org>
2016-11-20 15:09:28 -0800

    Update generic to handle default fn
    If fn is previously defined, set it as default fn. We still havent
    handled the case where a gen-fn is already defined

--- arguile/generic.scm
+++ arguile/generic.scm
@@ -1,10 +1,16 @@
 (module (arguile generic)
-    #:export (generic extend))
+    #:export (generic gen-fn? xtnd))
 (use (arguile base)
      (arguile data)
      (arguile guile)
      (arguile loop)
      (srfi srfi-1))
+
+(mac generic
+  ((_ name) (id? #'name)
+   #`(def name (%gen-fn 'name (ret t (make-tbl)
+                                #,(when (defd? (-> dat #'name))
+                                  #'(t 'fn name)))))))
 
 (data gen-fn (name tbl)
       #:init (%gen-fn name tbl)
@@ -12,26 +18,21 @@
               (apply (resolve-fn (gen-fn-tbl self) args)
                      args)))
 
-(mac generic
-  ((_ name) (id? #'name)
-   #'(def name (%gen-fn 'name (make-tbl)))))
-
-(mac extend
-  ((_ name (arg1 ...) body ...)
-   (defd? (-> dat #'name))
-   (w/syn ((args types) (split #'(arg1 ...)))
-     #'(loop ((for type (in-list 'types))
-              (where tbl (gen-fn-tbl name)
-                (let tbl* (tbl type)
-                  (if tbl* tbl*
-                      (do (tbl type (make-tbl))
-                          (tbl type))))))
-         => (tbl 'fn (fn args body ...))))))
-
 (def resolve-fn (tbl args)
   (loop ((for arg (in-list args))
          (where t tbl (and=> t (\\ _ (type arg)))))
-    => (if t (t 'fn) (err "No generic function for args:" args))))
+    => (if (& t (t 'fn)) (t 'fn)
+           (aif (tbl 'fn) it
+                (err "No generic fn for args:" args)))))
+
+(mac xtnd
+  ((_ name (arg1 ...) body ...) (defd? (-> dat #'name))
+   (let-syn (args types) (split #'(arg1 ...))
+     #'(loop ((for type  (in-list 'types))
+              (where tbl (gen-fn-tbl name)
+                (aif (tbl type) it
+                     (tbl type (make-tbl)))))
+         => (tbl 'fn (fn args body ...))))))
 
 (eval-when (expand load eval)
   (def split (lst)
