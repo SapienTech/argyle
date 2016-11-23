189f26c1da4d74b0216e3a317f7988dbfbf4a3b8
Dylan Jeffers <sapientech@openmailbox.org>
2016-11-22 23:26:32 -0800

    Add c/prmt mac

--- arguile/base/ctrl.scm
+++ arguile/base/ctrl.scm
@@ -1,8 +1,16 @@
 (module (arguile base ctrl))
 (use (arguile base)
      (ice-9 control))
-(re-export abort)
+(re-export abort (call/cc . c/cc) (call/ec . c/ec))
 
 (defp c/prmt call-with-prompt)
 (defp c/vals call-with-values)
+(defp tag default-prompt-tag)
 
+(mac $>
+  ((_ exp)           #'(c/prmt (tag) (fn () exp) hdlr))
+  ((_ exp hdlr)      #'(c/prmt (tag) (fn () exp) hdlr))
+  ((_ tag expr hdlr) #'(c/prmt (tag) (fn () exp) hdlr)))
+
+(def hdlr (cont f)
+  ($> (tag) (f cont) hdlr))
