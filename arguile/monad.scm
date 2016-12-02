b18da9d03ac6274f1d5a412381b99fc3565255af
Dylan Jeffers <sapientech@openmailbox.org>
2016-12-02 14:54:08 -0800

    Clean syntax

--- arguile/monad.scm
+++ arguile/monad.scm
@@ -1,45 +1,21 @@
 (module (arguile monad)
   #:export (;; Monads.
-            monad
-            monad?
-            monad-bind
-            monad-return
+            monad monad? monad-bind monad-return
 
             ;; Syntax.
-            >>=
-            return
-            w/monad
-            mlet
-            mlet*
-            mbegin
-            mwhen
-            munless
+            >>= return w/monad mlet mlet* mbegin mwhen munless
             lift0 lift1 lift2 lift3 lift4 lift5 lift6 lift7 lift
-            listm
-            foldm
-            mapm
-            sequence
-            anym
+            listm foldm mapm seq anym
 
             ;; Concrete monads.
-            %identity-monad
-
-            %state-monad
-            state-return
-            state-bind
-            current-state
-            set-current-state
-            state-push
-            state-pop
-            run-with-state))
+            %identity-monad %state-monad
+            state-return state-bind current-state set-current-state
+            state-push state-pop run-w/state))
 (use ((system syntax)
       #:select (syntax-local-binding))
-     (arguile base)
-     (arguile guile)
-     (arguile data)
-     (arguile loop)
+     (arguile base) (arguile guile) (arguile data) (arguile loop)
+     (arguile generic)
      (ice-9 match)
-     (srfi srfi-9)
      (srfi srfi-26))
 ;;; Commentary:
 ;;;
@@ -54,15 +30,18 @@
 ;;; Code:
 
 ;; Record type for monads manipulated at run time.
-(data <monad> (>>= return)
-      #:init (make-monad >>= return))      ; TODO: Add 'plus' and 'zero'
+(data monad (bind return)
+      #:init (mke-monad bind return))      ; TODO: Add 'plus' and 'zero'
 
 (mac monad (bind return)
   ((_ name (bind b) (return r))
-   (let-syn rtd (-> syn (+ (-> sym "% ") (-> dat #'name) '-rtd) #'name)
+   #'(def name (mke-monad b r))))
+#;(mac monad (bind return)
+  ((_ name (bind b) (return r))
+   (let-syn rtd (syn (+ (sym "% ") (dat #'name) '-rtd) #'name)
      #'(do
            ;; The data type, for use at run time.
-         (def rtd (make-monad b r))
+         (def rtd (mke-monad b r))
          (mac name (%bind %return)
                ;; An "inlined record", for use at expansion time.  The goal is
                ;; to allow 'bind' and 'return' to be resolved at expansion
@@ -72,16 +51,15 @@
                ((_ %return) #'r)
                ((_)         #'rtd))))))
 
-(define-syntax-parameter >>=
-  (lambda (s)
+(syn-param >>=
+  (fn (s)
     (syntax-violation '>>= ">>= (bind) used outside of 'w/monad'" s)))
 
-(define-syntax-parameter return
-  (lambda (s)
+(syn-param return
+  (fn (s)
     (syntax-violation 'return "return used outside of 'w/monad'" s)))
 
-(define-syntax-rule (bind-syntax bind)
-  "Return a macro transformer that handles the expansion of '>>=' expressions
+#;"Return a macro transformer that handles the expansion of '>>=' expressions
 using BIND as the binary bind operator.
 
 This macro exists to allow the expansion of n-ary '>>=' expressions, even
@@ -90,143 +68,122 @@ though BIND is simply binary, as in:
   (w/monad %state-monad
     (>>= (return 1)
          (lift 1+ %state-monad)
-         (lift 1+ %state-monad)))
-"
-  (lambda (stx)
-    (define (expand body)
-      (syntax-case body ()
-        ((_ mval mproc)
-         #'(bind mval mproc))
-        ((x mval mproc0 mprocs (... ...))
-         (expand #'(>>= (>>= mval mproc0)
-                        mprocs (... ...))))))
+         (lift 1+ %state-monad))) "
+(mac bind-syntax
+  ((_ bind)
+   #'(fn (stx)
+       (def (expand body)
+           (syntax-case body ()
+             ((_ mval mproc)
+              #'(bind mval mproc))
+             ((x mval mproc0 mprocs (... ...))
+              (expand #'(>>= (>>= mval mproc0)
+                             mprocs (... ...))))))
 
-    (expand stx)))
+       (expand stx))))
 
 (mac w/monad
   ;; "Evaluate BODY in the context of MONAD, and return its result."
   ((_ monad body ...)
-   (eq? 'macro (syntax-local-binding #'monad))
-   ;; MONAD is a syntax transformer, so we can obtain the bind and return
-   ;; methods by directly querying it.
-   #'(syntax-parameterize ((>>=    (bind-syntax (monad %bind)))
-                           (return (identifier-syntax (monad %return))))
-       body ...))
-  ((_ monad body ...)
-   ;; MONAD refers to the <monad> record that represents the monad at run
-   ;; time, so use the slow method.
-   #'(syntax-parameterize ((>>=    (bind-syntax
-                                    (monad-bind monad)))
+   #'(w/syn-params ((>>=    (bind-syntax
+                                    (monad->>= monad)))
                            (return (identifier-syntax
                                     (monad-return monad))))
        body ...)))
-
-(define-syntax mlet*
-  (syntax-rules (->)
-    "Bind the given monadic values MVAL to the given variables VAR.  When the
+(mac mlet* (->)
+#;"Bind the given monadic vals MVAL to the given variables VAR.  When the
 form is (VAR -> VAL), bind VAR to the non-monadic value VAL in the same way as
 'let'."
-    ;; Note: the '->' symbol corresponds to 'is:' in 'better-monads.rkt'.
-    ((_ monad () body ...)
-     (w/monad monad body ...))
-    ((_ monad ((var mval) rest ...) body ...)
-     (w/monad monad
-       (>>= mval
-            (lambda (var)
-              (mlet* monad (rest ...)
-                body ...)))))
-    ((_ monad ((var -> val) rest ...) body ...)
-     (_let ((var val))
-       (mlet* monad (rest ...)
-         body ...)))))
+  ((_ monad () body ...)
+   #'(w/monad monad body ...))
+  ((_ monad ((var mval) rest ...) body ...)
+   #'(w/monad monad
+              (>>= mval
+                   (fn (var)
+                     (mlet* monad (rest ...)
+                       body ...)))))
+  ((_ monad ((var -> val) rest ...) body ...)
+   #'(_let ((var val))
+           (mlet* monad (rest ...)
+             body ...))))
 
-(define-syntax mlet
-  (lambda (s)
-    (syntax-case s ()
-      ((_ monad ((var mval ...) ...) body ...)
-       (with-syntax (((temp ...) (generate-temporaries #'(var ...))))
-         #'(mlet* monad ((temp mval ...) ...)
-             (_let ((var temp) ...)
-               body ...)))))))
+(mac mlet
+  ((_ monad ((var mval ...) ...) body ...)
+   (let-syn (temp ...) (gen-tmps #'(var ...))
+            #'(mlet* monad ((temp mval ...) ...)
+                (_let ((var temp) ...)
+                      body ...)))))
 
-(define-syntax mbegin
-  (syntax-rules (%current-monad)
-    "Bind the given monadic expressions in sequence, returning the result of
+(mac mbegin 
+  #; "Bind the given monadic expressions in seq, returning the result of
 the last one."
-    ((_ %current-monad mexp)
-     mexp)
-    ((_ %current-monad mexp rest ...)
-     (>>= mexp
-          (lambda (unused-value)
+  ((_ %current-monad mexp) #'mexp)
+  ((_ %current-monad mexp rest ...)
+   #'(>>= mexp
+          (fn (unused-value)
             (mbegin %current-monad rest ...))))
-    ((_ monad mexp)
-     (w/monad monad
-       mexp))
-    ((_ monad mexp rest ...)
-     (w/monad monad
-       (>>= mexp
-            (lambda (unused-value)
-              (mbegin monad rest ...)))))))
+  ((_ monad mexp)
+   #'(w/monad monad mexp))
+  ((_ monad mexp rest ...)
+   #'(w/monad monad
+              (>>= mexp
+                   (fn (unused-value)
+                     (mbegin monad rest ...))))))
 
-(define-syntax mwhen
-  (syntax-rules ()
-    "When CONDITION is true, evaluate EXP0..EXP* as in an 'mbegin'.  When
+(mac mwhen
+  #;"When CONDITION is true, evaluate EXP0..EXP* as in an 'mbegin'.  When
 CONDITION is false, return *unspecified* in the current monad."
-    ((_ condition exp0 exp* ...)
-     (if condition
+  ((_ condition exp0 exp* ...)
+   #'(if condition
          (mbegin %current-monad
            exp0 exp* ...)
-         (return *unspecified*)))))
+         (return *unspecified*))))
 
-(define-syntax munless
-  (syntax-rules ()
-    "When CONDITION is false, evaluate EXP0..EXP* as in an 'mbegin'.  When
-CONDITION is true, return *unspecified* in the current monad."
-    ((_ condition exp0 exp* ...)
-     (if condition
+(mac munless
+  #; "When CONDITION is false, evaluate EXP0..EXP* as in an 'mbegin'.  When
+  CONDITION is true, return *unspecified* in the current monad."
+  ((_ condition exp0 exp* ...)
+   #'(if condition
          (return *unspecified*)
          (mbegin %current-monad
-           exp0 exp* ...)))))
+           exp0 exp* ...))))
 
-(define-syntax define-lift
-  (syntax-rules ()
-    ((_ liftn (args ...))
-     (define-syntax liftn
-       (lambda (s)
-         "Lift PROC to MONAD---i.e., return a monadic function in MONAD."
-         (syntax-case s ()
-           ((liftn proc monad)
-            ;; Inline the result of lifting PROC, such that 'return' can in
-            ;; turn be open-coded.
-            #'(lambda (args ...)
-                (w/monad monad
-                  (return (proc args ...)))))
-           (id
-            (identifier? #'id)
-            ;; Slow path: Return a closure-returning procedure (we don't
-            ;; guarantee (eq? LIFTN LIFTN), but that's fine.)
-            #'(lambda (proc monad)
-                (lambda (args ...)
-                  (w/monad monad
-                    (return (proc args ...))))))))))))
+(mac def-lift
+  ((_ liftn (args ...))
+   #'(mac liftn
+       #; "Lift PROC to MONAD---i.e., return a monadic function in MONAD."
+       ((liftn proc monad)
+        ;; Inline the result of lifting PROC, such that 'return' can in
+        ;; turn be open-coded.
+        #'(fn (args ...)
+            (w/monad monad
+                     (return (proc args ...)))))
+       ((liftn id)
+        (identifier? #'id)
+        ;; Slow path: Return a closure-returning procedure (we don't
+        ;; guarantee (eq? LIFTN LIFTN), but that's fine.)
+        #'(fn (proc monad)
+            (fn (args ...)
+              (w/monad monad
+                       (return (proc args ...)))))))))
 
-(define-lift lift0 ())
-(define-lift lift1 (a))
-(define-lift lift2 (a b))
-(define-lift lift3 (a b c))
-(define-lift lift4 (a b c d))
-(define-lift lift5 (a b c d e))
-(define-lift lift6 (a b c d e f))
-(define-lift lift7 (a b c d e f g))
+(def-lift lift0 ())
+(def-lift lift1 (a))
+(def-lift lift2 (a b))
+(def-lift lift3 (a b c))
+(def-lift lift4 (a b c d))
+(def-lift lift5 (a b c d e))
+(def-lift lift6 (a b c d e f))
+(def-lift lift7 (a b c d e f g))
 
-(define (lift proc monad)
+(def lift (proc monad)
   "Lift PROC, a procedure that accepts an arbitrary number of arguments, to
 MONAD---i.e., return a monadic function in MONAD."
-  (lambda args
+  (fn args
     (w/monad monad
       (return (apply proc args)))))
 
-(define (foldm monad mproc init lst)
+(def foldm (monad mproc init lst)
   "Fold MPROC over LST and return a monadic value seeded by INIT.
 
   (foldm %state-monad (lift2 cons %state-monad) '() '(a b c))
@@ -240,44 +197,46 @@ MONAD---i.e., return a monadic function in MONAD."
          (return result))
         ((head tail ...)
          (>>= (mproc head result)
-              (lambda (result)
+              (fn (result)
                 (lp tail result))))))))
 
-(define (mapm monad mproc lst)
+(def mapm (monad mproc lst)
   "Map MPROC over LST and return a monadic list.
 
   (mapm %state-monad (lift1 1+ %state-monad) '(0 1 2))
   => (1 2 3)  ;monadic
 "
   (mlet monad ((result (foldm monad
-                              (lambda (item result)
+                              (fn (item result)
                                 (>>= (mproc item)
-                                     (lambda (item)
+                                     (fn (item)
                                        (return (cons item result)))))
                               '()
                               lst)))
-    (return (reverse result))))
+    (return (rev result))))
 
-(define-syntax-rule (sequence monad lst)
-  "Turn the list of monadic values LST into a monadic list of values, by
-evaluating each item of LST in sequence."
-  ;; XXX: Making it a macro is a bit brutal as it leads to a lot of code
-  ;; duplication.  However, it allows >>= and return to be open-coded, which
-  ;; avoids struct-ref's to MONAD and a few closure allocations when using
-  ;; %STATE-MONAD.
-  (w/monad monad
-    (loop seq ((lstx   lst)
-               (result '()))
-      (match lstx
-        (()
-         (return (reverse result)))
-        ((head . tail)
-         (>>= head
-              (lambda (item)
-                (seq tail (cons item result)))))))))
+  #;
+"Turn the list of monadic vals LST into a monadic list of vals, by
+evaluating each item of LST in seq."
+;; XXX: Making it a macro is a bit brutal as it leads to a lot of code
+;; duplication.  However, it allows >>= and return to be open-coded, which
+;; avoids struct-ref's to MONAD and a few closure allocations when using
+;; %STATE-MONAD.
+(mac seq
+  ((_ monad lst)
+   #'(w/monad monad
+       (loop seq ((lstx   lst)
+                  (result '()))
+             (match lstx
+               (()
+                (return (rev result)))
+               ((head . tail)
+                (>>= head
+                     (fn (item)
+                       (seq tail (cons item result))))))))))
 
-(define (anym monad mproc lst)
-  "Apply MPROC to the list of values LST; return as a monadic value the first
+(def anym (monad mproc lst)
+  "Apply MPROC to the list of vals LST; return as a monadic value the first ;
 value for which MPROC returns a true monadic value or #f.  For example:
 
   (anym %state-monad (lift1 odd? %state-monad) '(0 1 2))
@@ -285,24 +244,22 @@ value for which MPROC returns a true monadic value or #f.  For example:
 "
   (w/monad monad
     (loop lp ((lst lst))
-      (match lst
-        (()
-         (return #f))
-        ((head tail ...)
-         (>>= (mproc head)
-              (lambda (result)
-                (if result
-                    (return result)
-                    (lp tail)))))))))
+          (match lst
+            (()
+             (return #f))
+            ((head tail ...)
+             (>>= (mproc head)
+                  (fn (result)
+                    (if result
+                        (return result)
+                        (lp tail)))))))))
 
-(define-syntax listm
-  (lambda (s)
-    "Return a monadic list in MONAD from the monadic values MVAL."
-    (syntax-case s ()
-      ((_ monad mval ...)
-       (with-syntax (((val ...) (generate-temporaries #'(mval ...))))
-         #'(mlet monad ((val mval) ...)
-             (return (list val ...))))))))
+(mac listm
+  #; "Return a monadic list in MONAD from the monadic vals MVAL."
+  ((_ monad mval ...)
+   (let-syn (val ...) (gen-tmps #'(mval ...))
+            #'(mlet monad ((val mval) ...)
+                (return (list val ...))))))
 
 
 
@@ -310,32 +267,32 @@ value for which MPROC returns a true monadic value or #f.  For example:
 ;;; Identity monad.
 ;;;
 
-(define-inlinable (identity-return value)
+(inline identity-return (value)
   value)
 
-(define-inlinable (identity-bind mvalue mproc)
+(inline identity-bind (mvalue mproc)
   (mproc mvalue))
 
 (monad %identity-monad
-  (bind   identity-bind)
-  (return identity-return))
+       (bind identity-bind)
+       (return identity-return))
 
 
 ;;;
 ;;; State monad.
 ;;;
 
-(define-inlinable (state-return value)
-  (lambda (state)
-    (values value state)))
+(inline state-return (value)
+  (fn (state)
+    (vals value state)))
 
-(define-inlinable (state-bind mvalue mproc)
+(inline state-bind (mvalue mproc)
   "Bind MVALUE, a value in the state monad, and pass it to MPROC."
-  (lambda (state)
-    (call-with-values
-        (lambda ()
+  (fn (state)
+    (c/vals
+        (fn ()
           (mvalue state))
-      (lambda (value state)
+      (fn (value state)
         ;; Note: as of Guile 2.0.11, declaring a variable to hold the result
         ;; of (mproc value) prevents a bit of unfolding/inlining.
         ((mproc value) state)))))
@@ -344,34 +301,32 @@ value for which MPROC returns a true monadic value or #f.  For example:
   (bind state-bind)
   (return state-return))
 
-(define* (run-with-state mval #:optional (state '()))
+(def run-w/state (mval #:o(state '()))
   "Run monadic value MVAL starting with STATE as the initial state.  Return
-two values: the resulting value, and the resulting state."
+two vals: the resulting value, and the resulting state."
   (mval state))
 
-(define-inlinable (current-state)
+(inline current-state ()
   "Return the current state as a monadic value."
-  (lambda (state)
-    (values state state)))
+  (fn (state)
+    (vals state state)))
 
-(define-inlinable (set-current-state value)
+(inline set-current-state (value)
   "Set the current state to VALUE and return the previous state as a monadic
 value."
-  (lambda (state)
-    (values state value)))
+  (fn (state)
+    (vals state value)))
 
-(define (state-pop)
+(def state-pop ()
   "Pop a value from the current state and return it as a monadic value.  The
 state is assumed to be a list."
-  (lambda (state)
+  (fn (state)
     (match state
       ((head . tail)
-       (values head tail)))))
+       (vals head tail)))))
 
-(define (state-push value)
+(def state-push (value)
   "Push VALUE to the current state, which is assumed to be a list, and return
 the previous state as a monadic value."
-  (lambda (state)
-    (values state (cons value state))))
-
-;;; monads.scm end here
+  (fn (state)
+    (vals state (cons value state))))
