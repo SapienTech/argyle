392c57639c2a5adfcd7c08ed2acb436a3f049ec8
Dylan Jeffers <sapientech@openmailbox.org>
2016-12-23 11:10:59 -0800

    Add bracket vector support

--- arguile/reader.scm
+++ arguile/reader.scm
@@ -1,3 +1,24 @@
-(module (arguile reader))
+(define-module (arguile reader))
 
+;;; Will use default scm until i add my own arguile geiser file...
+
+;;; First assuming that we can't do inner quoting
+;;; Doesn't work to quote things sequentially?
+(read-hash-extend
+ #\[
+ (lambda (chr prt)
+   (let lp ((acc '(#\( #\#)))
+     (let ((nxt (read-char prt)))
+       (case nxt
+         ((#\]) (string->symbol
+                 (list->string
+                  (reverse (cons #\) acc)))))
+         (else (lp (cons nxt acc))))))))
+
+(read-hash-extend
+ #\y
+ (lambda (chr prt)
+   "(* 2 3)"))
+
+;;; The ones that i want: vector and map rules
 ;;; TODO: build some sweet reader macros
