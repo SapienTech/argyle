aaf633fa98723b396af4b283521c9d10988ef8f4
Dylan Jeffers <sapientech@openmailbox.org>
2017-01-05 21:52:21 -0800

    Update conc, Add to arguile

--- arguile.scm
+++ arguile.scm
@@ -3,6 +3,7 @@
      (arguile data)
      (arguile generic)
      (arguile loop)
+     (arguile conc)
      (arguile reader)
      
      (ice-9 match)
@@ -14,6 +15,7 @@
  (arguile data)
  (arguile generic)
  (arguile loop)
+ (arguile conc)
  
  (ice-9 match)
  (ice-9 receive)
--- arguile/conc.scm
+++ arguile/conc.scm
@@ -1,5 +1,5 @@
 (module (arguile conc)
-    #:export (futr doasync dosync ref ref-val @))
+    #:export (futr futr? mke-futr <ref> ref ref? @ doasync dosync alter))
 (use (arguile base)
      (arguile generic)
      (arguile guile)
@@ -12,37 +12,47 @@
 (mac futr
   ((_ exp) #'(future exp)))
 
-(defp @ touch)
-
-(defp mke-future make-future)
-
-(mac doasync
-  ((_ e0 ...) #'(parallel e0 ...)))
+(defp futr? future?)
+(defp mke-futr make-future)
 
 (data ref (val mutx)
       #:init (%ref val mutx))
 
 (defp ref (val) (%ref val (make-mutex)))
 
+(defp @ (lzy-obj)
+    (cond ((futr? lzy-obj) (touch lzy-obj))
+          ((ref? lzy-obj) (ref-val lzy-obj))
+          (else (err "attempt to deref a non-lazy obj" lzy-obj))))
+
+(mac doasync
+  ((_ e0 ...) #'(parallel e0 ...)))
+
 (mac dosync x
-     ((_ (refs ...) body ...) 
-      (let ref-dats (map (fn (ref) (syn->dat ref)) #'(refs ...))
-        (with-syntax (((ref-cpys ...) (map (fn (ref) (datum->syntax x ref))
-                                           ref-dats)))
-          #`(w/refs (refs ...)
-              (let refs* (_let #,(zip #'(ref-cpys ...)
-                                      (map (fn (ref) #`(cpy-ref #,ref))
-                                           #'(refs ...)))
-                               body ...
-                               (list ref-cpys ...))
-                (set-refs! (refs ...) refs*)))))))
+  ((_ (refs ...) body ...) 
+   (let ref-dats (map (fn (ref) (syn->dat ref)) #'(refs ...))
+     (w/syn ((ref-cpys ...) (map (fn (ref) (datum->syntax x ref)) ref-dats)
+             alter (syn 'alter x))
+       #`(w/refs (refs ...)
+           (let refs* (_let #,(zip #'(ref-cpys ...)
+                                   (map (fn (ref) #`(cpy-ref #,ref))
+                                        #'(refs ...)))
+                            body ...
+                            (list ref-cpys ...))
+             (set-refs! (refs ...) refs*)))))))
+
+;;; TODO: only allow user to use alter in a dosync block
+(mac alter
+  ((_ ref val) 
+   #'(with-mutex (ref-mutx ref)
+       (set! ref (ref-val! ref val)))))
 
 (mac set-refs!
-     ((_ (refs ...) vals)
-      #`(do #,@(map (fn (ref val) #`(set! #,ref (ref-val! #,ref (ref-val #,val))))
-                    #'(refs ...)
-                    #`(#,@(map (fn (i) #`(list-ref vals #,i))
-                               (iota (_length #'(refs ...)))))))))
+  ((_ (refs ...) vals)
+   #`(do #,@(map (fn (ref val) #`(set! #,ref (ref-val! #,ref (ref-val #,val))))
+                 #'(refs ...)
+                 #`(#,@(map (fn (i) #`(list-ref vals #,i))
+                            (iota (_length #'(refs ...)))))))))
 
 (mac w/refs
   ((_ (r1 ...) e1 ...)
@@ -53,14 +63,10 @@
 (mac w/mutxs
   ((_ (m1 ...) e1 ...)
    #`(#,@(loop lp ((ms #'(m1 ...)))
-               (if (nil? ms) #'(do e1 ...)
-                   #`(with-mutex #,(car ms)
-                       #,(lp (cdr ms))))))))
+           (if (nil? ms) #'(do e1 ...)
+               #`(with-mutex #,(car ms)
+                   #,(lp (cdr ms))))))))
 
-(mac alter
-  ((_ ref val) 
-   #'(with-mutex (ref-mutx ref)
-       (set! ref (ref-val! ref val))))) 
 
 (eval-when (expand load eval)
   (def cpy-ref (ref) (ref-mutx! ref (make-mutex))))
