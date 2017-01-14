3b5e843d583a66e04cbadc0d5583e7c68ac29850
Dylan Jeffers <sapientech@openmailbox.org>
2017-01-13 23:40:48 -0800

    Add mlet mw/ match-xpnd, Refactor mdef

--- arguile/match.scm
+++ arguile/match.scm
@@ -1,14 +1,26 @@
 (module (arguile match))
 (use (arguile base)
-     (ice-9 match))
+     (arguile match compat)
+     ((srfi srfi-1) :select (zip reduce)))
 
 (mac mdef
-  (def match-args (args mtchrs bdy)
-    (if (nil? args) #`(do #,@bdy)
-        #`(match #,(car args)
-            (#,(car mtchrs)
-             #,(match-args (cdr args) (cdr mtchrs) bdy)))))
-  ((_ name (a0 ...) b0 b1 ...)
-   (let tmps (gen-tmps #'(a0 ...))
-     #`(def name #,tmps
-         #,(match-args tmps #'(a0 ...) #'(b0 b1 ...))))))
+  (def splice (lst)                     ;TODO: use general ver
+    (reduce append '() (reverse lst)))
+  ((_ name (pat ...) . bdy)
+   (let-syn exps (gen-tmps #'(pat ...))
+     #`(def name exps
+         (match-xpnd #,(splice (zip #'(pat ...) #'exps)) . bdy)))))
+
+(mac mlet
+  ((_ pat exp . bdy) #'(mw/ (pat exp) . bdy)))
+
+(mac mw/
+  ((_ pat/exp . bdy) #'(match-xpnd pat/exp . bdy)))
+
+(mac match-xpnd
+  ((_ () . bdy)
+   #'(do . bdy))
+  ((_ (pat exp . rst) . bdy)
+   #'(match exp (pat (match-xpnd rst . bdy)))))
+
+
